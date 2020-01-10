71ff6e2ff80249ba233f500ceda703f30ddfedb7
PackYokohama <59277976+PackYokohama@users.noreply.github.com>
2020-01-10 17:12:07 +0700

    Update rehearsal_2.cpp

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,17 @@
-
+#include <iostream>
+using namespace std;
+int main(){
+	int sum = 0;
+	double x;
+	cout << "Enter X:" + x "\n";
+	cin >> x;
+	while(x = 0){
+	cout << sum;}
+	else(x > 0){
+	cout << sum+=x;}
+	else{
+		cin >> x;
+	}
+	
+	return 0;
+}
