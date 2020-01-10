263e4e318b1d6e99cff7385cb37e4815cec4df55
PackYokohama <59277976+PackYokohama@users.noreply.github.com>
2020-01-10 17:11:38 +0700

    Update rehearsal_1.cpp

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string name;
+	double GPA;
+	
+	cout << "what is your name?:\n";
+	cin >> name;
+	cout << "what is your GPA?:\n";
+	cin >> GPA;
+	if (GPA >= 3.50){
+    cout << name+"InW Jrim Jrim!!!";}
+	else{
+	cout << "Try harder,"+name+"!!!";}
+	return 0;
+}
 
